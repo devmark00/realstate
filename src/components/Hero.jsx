9259d5db870f64851664c5d2814e1b8aeecffa23
devmark00 <abhinavkrmishra7524@gmail.com>
2024-10-02 23:42:48 +0530

    create navbar component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='hero text-blue-500 font-bold'>
+      This is hero page for my real state website.
+    </div>
+  )
+}
+
+export default Hero
